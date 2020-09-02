7cb99ce2df354be3667f5669de356c19ae7acd4c
Hector <hmreluz@gmail.com>
2020-09-02 23:36:53 +1000

    Creating Model and Migration for Notes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,8 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    {   
+        //https://www.oreilly.com/library/view/full-stack-vuejs-2/9781788299589/30f2198e-5e56-4a32-a72a-97cea5030641.xhtml 
+        Schema::defaultStringLength(191);
     }
 }
